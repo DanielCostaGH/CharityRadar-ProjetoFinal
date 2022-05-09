972cd72e3ff82fae55663d4a24c4a7ae84fa79d1
DanielCostaGH <dfscs.costa@gmail.com>
2022-05-09 19:34:22 -0300

    arquivo auth update

--- control/auth.js
+++ control/auth.js
@@ -2,38 +2,48 @@ const localstrategy = require('passport-local').Strategy;
 const bcrypt= require('bcryptjs');
 //models e dados 
 const models= require('../Banco de Dados/models/db');
-const usuario = require('../Banco de Dados/models/PostUser');
+const PostUser = require('../Banco de Dados/models/PostUser');
 //--------
 
 //logica do login no js
+/**
+ * 
+ * @param {import("passport")} passport 
+ */
 module.exports = function(passport){
-    passport.use(new localstrategy({ usernameField : 'email',passwordField: 'senha'},(email,senha,done)=>{
-        usuario.findOne({email: email}).then((usuario) =>{
+    passport.use(new localstrategy({ usernameField : 'email',passwordField: 'senha'}, (email,senha,done)=>{
+        PostUser.findOne({
+            where: {email: email}
+           
+        }).then((ref) =>{
+            if(!ref){
+                return done (null,false,{message: ' Credenciais inválidas'})
+            }
+            const usuario = ref.toJSON();
+            
+            console.log(usuario, usuario.senha);
             if(!usuario){
                 return done (null,false,{message: ' Credenciais inválidas'})
             }
 
             bcrypt.compare(senha,usuario.senha,(erro,bate) =>{
                 if(bate){
-                    return done (null,usuario);
+                    done(null,{id: usuario.id });
                 }else{
                     return done(null,false,{message: " Credenciais inválidas senha"})
                 }
             })
 
         })
-    }))
+    })) 
 
-    passport.serializeUser((usuario,done) =>{
-        done(null,usuario.id)
-    })
-
-    passport.deserializeUser((id,done)=>{
-        usuario.findById(id,(err,usuario)=>{
-            done(err,usuario);
-        })
-    })
-          
+    passport.serializeUser((user, done) => {
+        done(null, user);
+    });
+      
+    passport.deserializeUser(function(user, done) {
+        done(null, user);
+    });      
 }
           
 //---------------------
@@ -68,31 +78,4 @@ module.exports = function(passport){
           
           
           
-            /* if(!usuario){
-                return done (null,false,{msg:" Usuário não encontrado !"});
-            }
-            const res = bcrypt.compare(senha,usuario.senha,(err,resposta)=>{
-                if(!resposta){
-                    return done(null,usuario);
-                }else{
-                    return done(null,false,{msg: " Erro ao logar !" + err});
-                }
-            });
-
-        });
-    }));
-    passaport.serializeUser((usuario,done)=>{
-        done(null,usuario.id);
-    });
-    passaport.deserializerUser((id,done)=>{
-        Usuario.findOne({where:{id:id}}).then((res)=>{
-            if(res){
-                return done(null,false,{msg: " Não foi encontrado "});
-            }else{
-                done(null,res);
-            }
-        
-        })
-    });
-
-};*/
+     
