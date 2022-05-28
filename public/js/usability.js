a8e570f7e834b8746f5dcd84fcc9c7fa77509c3c
DanielCostaGH <dfscs.costa@gmail.com>
2022-05-28 13:25:23 -0300

    SideBar atualizada e funcional

--- public/js/usability.js
+++ public/js/usability.js
@@ -1,86 +1,82 @@
 const logadoounao = document.getElementById('nav-linkblue').innerText;
-if(logadoounao == '' || logadoounao == "Acesse sua conta"){
-const container = document.querySelector(".container"),
-      pwShowHide = document.querySelectorAll(".showHidePw"),
-      pwFields = document.querySelectorAll(".password"),
-      signUp = document.querySelector(".signup-link"),
-      login = document.querySelector(".login-link");
+if (logadoounao == '' || logadoounao == "Acesse sua conta") {
+    const container = document.querySelector(".container"),
+        pwShowHide = document.querySelectorAll(".showHidePw"),
+        pwFields = document.querySelectorAll(".password"),
+        signUp = document.querySelector(".signup-link"),
+        login = document.querySelector(".login-link");
 
-    pwShowHide.forEach(eyeIcon =>{
-        eyeIcon.addEventListener("click", ()=>{
-            pwFields.forEach(pwField =>{
-                if(pwField.type ==="password"){
+    pwShowHide.forEach(eyeIcon => {
+        eyeIcon.addEventListener("click", () => {
+            pwFields.forEach(pwField => {
+                if (pwField.type === "password") {
                     pwField.type = "text";
 
-                    pwShowHide.forEach(icon =>{
+                    pwShowHide.forEach(icon => {
                         icon.classList.replace("uil-eye-slash", "uil-eye");
                     })
-                }else{
+                } else {
                     pwField.type = "password";
 
-                    pwShowHide.forEach(icon =>{
+                    pwShowHide.forEach(icon => {
                         icon.classList.replace("uil-eye", "uil-eye-slash");
                     })
                 }
-            }) 
+            })
         })
     })
 
-    signUp.addEventListener("click", ( )=>{
+    signUp.addEventListener("click", () => {
         container.classList.add("active");
     });
-    login.addEventListener("click", ( )=>{
+    login.addEventListener("click", () => {
         container.classList.remove("active");
     });
-// ================================================= PUXAR MODAL FUNCTIONS =====================================
+    // ================================================= PUXAR MODAL FUNCTIONS =====================================
 
-   
-        function iniciaModal(modalID){
+
+    function iniciaModal(modalID) {
         const modal = document.getElementById(modalID);
         modal.classList.add('mostrar');
-        modal.addEventListener('click', (e)=> {
-            if(e.target.id == modalID || e.target.className == 'uil uil-multiply'){
+        modal.addEventListener('click', (e) => {
+            if (e.target.id == modalID || e.target.className == 'uil uil-multiply') {
                 modal.classList.remove('mostrar')
             }
         })
-        }
+    }
 
-        const accAcess = document.querySelector('.accAcess');
-        accAcess.addEventListener('click', () => 
+    const accAcess = document.querySelector('.accAcess');
+    accAcess.addEventListener('click', () =>
         iniciaModal('modal-popup'));
-        
-// ================================================= PUXAR BARRA LATERAL =====================================
 
-}else {
-    function iniciaModal(modalID){
-        const modal = document.getElementById(modalID);
-        modal.classList.add('mostrar');
-        modal.classList.add('option-show');
-        modal.addEventListener('click', (e)=> {
-            if(e.target.id == modalID){
-                modal.classList.remove('mostrar')
-            }
-        })
-        }
+    // ================================================= PUXAR BARRA LATERAL =====================================
 
-        const accAcess = document.querySelector('.accAcess');
-        accAcess.addEventListener('click', () => 
-        iniciaModal('modal-barId'));
+} else {
+
+    const Rmodal = document.getElementById('nav-linkblue');
+    Rmodal.classList.add('hide');
+
+
+
+
+    const modalbar = document.getElementById('nav-linkblue2')
+    modalbar.classList.remove('hide')
 }
+
 
 
 // ======================= MODAL EDITAR CADASTRO =====================
 
-function iniciaModalCadastro(modalID){
+function iniciaModalCadastro(modalID) {
     const modal = document.getElementById(modalID);
     modal.classList.add('mostrar');
-    modal.addEventListener('click', (e)=> {
-        if(e.target.id == modalID || e.target.className == 'uil uil-multiply'){
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == modalID || e.target.className == 'uil uil-multiply') {
             modal.classList.remove('mostrar')
         }
     })
-    }
+}
 
-    const accAcess = document.querySelector('.modal-editCadastro');
-    accAcess.addEventListener('click', () => 
+const accAcess = document.querySelector('.modal-editCadastro');
+accAcess.addEventListener('click', () =>
     iniciaModalCadastro('modal-edCadastro'));
